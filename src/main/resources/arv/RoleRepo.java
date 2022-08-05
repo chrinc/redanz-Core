c25fa3a0da844dfae028a974a62b49350381d052
chrinc <zeus522@gmail.com>
2022-08-05 10:43:03 +0200

    inc, update Readme

--- src/main/resources/arv/RoleRepo.java
+++
@@ -1,8 +0,0 @@
-//package ch.redanz.redanzCore.repo;
-//
-//import ch.redanz.redanzCore.user.Role;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface RoleRepo extends JpaRepository<Role, Long> {
-//  Role findRoleByInternalId(String internalId);
-//}
