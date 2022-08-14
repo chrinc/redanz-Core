30332705e221c158ed94a05dbc06d045de4574ff
chrinc <zeus522@gmail.com>
2022-08-15 00:44:28 +0200

    inc, update reports hosting / accommodation

--- src/main/java/ch/redanz/redanzCore/model/registration/repository/HostRegistrationRepo.java
+++ src/main/java/ch/redanz/redanzCore/model/registration/repository/HostRegistrationRepo.java
@@ -2,8 +2,12 @@ package ch.redanz.redanzCore.model.registration.repository;
 
 import ch.redanz.redanzCore.model.registration.entities.HostRegistration;
 import ch.redanz.redanzCore.model.registration.entities.Registration;
+import ch.redanz.redanzCore.model.workshop.entities.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface HostRegistrationRepo extends JpaRepository<HostRegistration, Long> {
     HostRegistration findAllByRegistration(Registration registration);
+    List<HostRegistration> findAllByRegistrationEvent(Event event);
 }
