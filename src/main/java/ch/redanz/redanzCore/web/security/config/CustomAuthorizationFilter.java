0c7b4b42dbb5f0227a1319483ef021b766444c38
chrinc <zeus522@gmail.com>
2026-04-21 01:17:09 +0200

    inc, Release 10.704 / Refactor Static Data

--- src/main/java/ch/redanz/redanzCore/web/security/config/CustomAuthorizationFilter.java
+++ src/main/java/ch/redanz/redanzCore/web/security/config/CustomAuthorizationFilter.java
@@ -42,7 +42,8 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
     if(
-         request.getServletPath().equals("/core-api/login")
+         request.getServletPath().equals("/host-key")
+      || request.getServletPath().equals("/core-api/login")
       || request.getServletPath().equals("/core-api/login/token/refresh")
       || request.getServletPath().equals("/core-api/zahls/checkout/confirm")
       || request.getServletPath().equals("/core-api/payrexx/checkout/confirm")
